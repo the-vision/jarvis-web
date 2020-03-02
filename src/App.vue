4863668907bf005910594416671e68cd1d4dfb10
tanisha03 <tanisha031199@gmail.com>
2020-03-02 21:18:57 +0530

    Add basic template (#1)

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Navigation />
+    <LandingPage />
+  </div>
+</template>
+
+<script>
+import Navigation from "./components/Navigation.vue";
+import LandingPage from "./components/LandingPage.vue";
+
+export default {
+  name: "App",
+  components: {
+    LandingPage,
+    Navigation
+  }
+};
+</script>
+
+<style>
+body,
+html {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/components/LandingPage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="main">
+    <div class="split left">
+      <div class="centered">
+        <h2>About</h2>
+      </div>
+    </div>
+
+    <div class="split right">
+      <div class="centered">
+        <h2>Chat</h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LandingPage"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.split {
+  height: 100%;
+  overflow-x: hidden;
+  position: fixed;
+}
+.left {
+  left: 0;
+  background-color: #fafafa;
+  width: 30%;
+}
+.right {
+  right: 0;
+  background-color: #ececec;
+  width: 70%;
+  float: right;
+}
+</style>
