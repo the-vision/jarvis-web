794a2602b4c4c275672afa833b90931fb927d7d6
Tanisha <tanisha031199@gmail.com>
2020-03-02 19:04:09 +0530

    basic template

---
+++ src/components/Navigation.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="nav">
+    <h3 class="header">Jarvis</h3>
+    <span>
+      <a href="#">About</a>
+      <a href="#">Chat</a>
+    </span>
+  </div>
+</template>
+<script>
+export default {
+  name: "Navigation"
+};
+</script>
+<style scoped>
+.nav {
+  height: 4em;
+  background-color: #333;
+  padding: 1em;
+}
+.header {
+  color: #fff;
+  float: left;
+  margin: 0.5em;
+  font-size: 2em;
+}
+.nav a {
+  color: #fff;
+  float: right;
+  text-decoration: none;
+  font-size: 1.5em;
+  margin: 0.5em;
+}
+</style>
