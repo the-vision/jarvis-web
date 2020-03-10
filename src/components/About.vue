a80b3863a263b6590c0f0677f3d8d3b89cd369eb
Abhushan <abhushanadhikarijoshi@gmail.com>
2020-03-10 14:30:51 +0530

    Split the landing page into respective components

---
+++ src/components/About.vue
@@ -0,0 +1,23 @@
+<template>
+ <div class="split left">
+      <div class="centered">
+        <h2>About</h2>
+      </div>
+</div>
+</template>
+
+
+<script>
+export default {
+  name: "About"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.left {
+  left: 0;
+  background-color: #fafafa;
+  width: 30%;
+}
+</style>
