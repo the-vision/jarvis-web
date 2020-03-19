a3e5e4e61fdbeb05566e002a20692cf3b33c7eb8
Abhushan <abhushanadhikarijoshi@gmail.com>
2020-03-19 21:25:22 +0530

    Addition of responsive navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,25 @@
 <template>
     <div id="app">
         <GithubCorner url='https://github.com/the-vision/jarvis-web'
-                      :size='70' gitColor='#fff' cornerColor='#000' flipOnHover />
-        <div id="nav">
+                      :size='70' gitColor='#fff' cornerColor='#20639b' flipOnHover />
+        <Navigation/>
+        <!-- <div id="nav">
             <router-link to="/">Home</router-link> |
             <router-link to="/about">About</router-link>
-        </div>
+        </div> -->
         <router-view />
     </div>
 
 </template>
 
 <script>
-    import GithubCorner from 'vue-github-corners'
+    import GithubCorner from 'vue-github-corners';
+    import Navigation from './components/Navigation'
     export default {
         name: "App",
         components: {
-            GithubCorner
+            GithubCorner,
+            Navigation
         }
     };
 </script>
@@ -34,12 +37,12 @@
         padding: 30px;
     }
 
-        #nav a {
+    #nav a {
             font-weight: bold;
             color: #2c3e50;
-        }
+    }
 
-            #nav a.router-link-exact-active {
-                color: #42b983;
-            }
+    #nav a.router-link-exact-active {
+        color: #42b983;
+    }
 </style>
