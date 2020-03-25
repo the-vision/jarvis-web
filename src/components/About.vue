b048aa6d88bc1ff78593c70534ba4cff13b62795
divya-prakash-1 <medivyaprakash@gmail.com>
2020-03-25 08:20:34 +0530

    project details updated
    project details updated view in columns as suggested

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,23 +1,68 @@
 <template>
  <div class="split left">
-      <div class="centered">
-        <div class="row"><h1>1.Jarvis Telegram</h1></div>
-        <div class="row"><p>Wanna experience a very unique style of using Telegram, Jarvis Telegram is here for you.
-We are introducing the all new Jarvis in Telegram app which is capable of helping in picking best groups for you, suggestions, chats and much more with the power of AI.
-</p></div>
-<div class="row"><h1> 2.Jarvis Core</h1></div>
-<div class="row"><p>Jarvis Core is the main model which include all the codes which forms the mind of Jarvis. Built in python, it is a completely trained model
-   working behind all the services provided by Jarvis.</p></div>
-      </div>
-      <div class="row"><h1>3.Jarvis Slack</h1></div>
-      <div class="row"><p>Slack is the place where the organizations get an  unique way of managing their projects and a place.
-It is integrated with many apps and other services and communicate with them to provide all details at a place.
-We are introducing Jarvis Slack to provide our Jarvis experience in the slack app. Integrated with direct chats to Jarvis, it is built for helping you in managing stuffs in slack, auto-organize works and suggesting you in your works.
-</p></div>
-<div class="row"><h1>4.Jarvis Discord</h1></div>
-<div class="row"><p>Gaming has got a new level of experience after the introduction of Discord, a platform to communicate while playing games, hanging out with peoples, making connections and having fun.
-Jarvis Discord is here for you to make that experience more interesting. It is an integration of Jarvis in Discord platform. It is designed to help you in your games by use of AI. 
-</p></div>
+     <div class="row">
+        <div class="col">
+            <div class="row">
+                <div class="col">
+                    <h1><h1>1.Jarvis Telegram</h1></h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <p>
+                        Wanna experience a very unique style of using Telegram, Jarvis Telegram is here for you.
+                        We are introducing the all new Jarvis in Telegram app which is capable of helping in picking best groups for you, suggestions, chats and much more with the power of AI.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="row">
+                <div class="col">
+                    <h1> 2.Jarvis Core</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <p>
+                        Jarvis Core is the main model which include all the codes which forms the mind of Jarvis. Built in python, it is a completely trained model
+                        working behind all the services provided by Jarvis.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="row">
+                <div class="col">
+                    <h1>3.Jarvis Slack</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <p>
+                        Slack is the place where the organizations get an  unique way of managing their projects and a place.
+                        It is integrated with many apps and other services and communicate with them to provide all details at a place.
+                        We are introducing Jarvis Slack to provide our Jarvis experience in the slack app. Integrated with direct chats to Jarvis, it is built for helping you in managing stuffs in slack, auto-organize works and suggesting you in your works.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="row">
+                <div class="col">
+                    <h1>4.Jarvis Discord</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <p>
+                        Gaming has got a new level of experience after the introduction of Discord, a platform to communicate while playing games, hanging out with peoples, making connections and having fun.
+                        Jarvis Discord is here for you to make that experience more interesting. It is an integration of Jarvis in Discord platform. It is designed to help you in your games by use of AI.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 </template>
 
