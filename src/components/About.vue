098bdf35521e19e0b6a9b036cd57905456183519
Divya Prakash <medivyaprakash@gmail.com>
2020-04-04 20:17:52 +0530

    Add project details (#24)
    Close #15 
    
    * project details added
    
    * project details updated
    
    project details updated view in columns as suggested
    
    * project details updated with design

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,9 +1,57 @@
 <template>
- <div class="split left">
-      <div class="centered">
-        <h2>About</h2>
-      </div>
-</div>
+    <div class="split left">
+        <div class="row">
+            <div class="col d-inline-flex">
+                <div class="card shadow">
+                    <div class="card-body">
+                        <h1 class="card-title">Jarvis Telegram</h1>
+                        <p class="card-text">
+                            &nbsp;Wanna experience a very unique style of using Telegram, Jarvis Telegram is here for you.<br>We are introducing the all new Jarvis in Telegram app which is capable of helping in picking best groups for you, suggestions, chats
+                            and much more with the power of AI.<br>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col d-inline-flex">
+                <div class="card shadow">
+                    <div class="card-body">
+                        <h1 class="card-title">Jarvis Core</h1>
+                        <p class="card-text">Jarvis Core is the main model which include all the codes which forms the mind of Jarvis. Built in python, it is a completely trained model working behind all the services provided by Jarvis.<br></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col d-inline-flex">
+                <div class="card shadow">
+                    <div class="card-body">
+                        <h1 class="card-title">Jarvis Slack</h1>
+                        <p class="card-text">
+                            Slack is the place where the organizations get an&nbsp;unique way of managing their projects and a place.It is integrated with many apps and other services and communicate with them to provide all details at a place.<br>We are introducing
+                            Jarvis Slack to provide our Jarvis experience in the slack app. Integrated with direct chats to Jarvis, it is built for helping you in managing stuffs in slack, auto-organize works and suggesting you in your works.<br>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow">
+                    <div class="card-body d-inline-flex">
+                        <div class="row">
+                            <div class="col">
+                                <h1>Jarvis Discord</h1>
+                                <div class="row">
+                                    <div class="col">
+                                        <p>
+                                            &nbsp;Gaming has got a new level of experience after the introduction of Discord, a platform to communicate while playing games, hanging out with peoples, making connections and having fun.<br>Jarvis Discord is here
+                                            for you to make that experience more interesting. It is an integration of Jarvis in Discord platform. It is designed to help you in your games by use of AI.<br>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 
@@ -19,5 +67,18 @@ export default {
   left: 0;
   background-color: #fafafa;
   width: 30%;
+}
+p{
+    font-family:sans-serif
+
+}
+h1{
+    text-align:center;
+    font-family:cursive;
+    font-weight:400;
+
+}
+.card {
+    background: linear-gradient(0deg, rgba(123,198,255,1) 0%, rgba(107,175,229,1) 100%);
 }
 </style>
