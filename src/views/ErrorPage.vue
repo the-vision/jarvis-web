c550216250714ce156e527cdb7727729cc467b5c
Abhushan <abhushanadhikarijoshi@gmail.com>
2020-04-11 20:19:36 +0530

    Addition of 404 page

---
+++ src/views/ErrorPage.vue
@@ -0,0 +1,56 @@
+<template>
+<section class="error-page">
+    <h1>Oops! Something seems off!</h1>
+    <a href="/" class="anchor-link">Back to home</a>
+    <div class="graphics-container">
+        <ErrorDisplay class="graphics"/>
+    </div>
+</section>
+   
+</template>
+
+<script>
+import ErrorDisplay from '../components/ErrorDisplay';
+export default {
+    name:"ErrorPage",
+    components:{
+        ErrorDisplay
+    }
+}
+</script>
+
+<style  scoped>
+.error-page{
+    margin-top:2rem;
+    display:flex;
+    flex-direction:column;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.graphics-container{
+    flex:0 0 1;
+    width:40rem;
+    height:30rem;
+     margin-top:5rem;
+}
+.graphics{
+   width:100%;
+   height:100%;
+}
+
+.anchor-link:link,
+.anchor-link:visited{
+    text-transform: uppercase;
+    text-decoration: none;
+    color:#099456;
+    font-weight:bold;
+}
+
+.anchor-link:hover,
+.anchor-link:active{
+    color:#01502d;
+}
+
+
+</style>
