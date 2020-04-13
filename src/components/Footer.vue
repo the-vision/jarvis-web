b03075bb7d74bcc3645495f7e3565fd94d794281
tanisha03 <tanisha031199@gmail.com>
2020-04-13 12:21:41 +0530

    UI changes (#30)
    * UI changes
    
    * UI changes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,104 +1,100 @@
 <template>
- <footer>
-     <div class="footer">
-        <div>
-           <ul class="footer__links">
-                <h4 class="footer__head">Information</h4>
-                <li class="footer__links-item">
-                    <a class="footer__link" href="#!">Home</a>
-                </li>
-                <li class="footer__links-item">
-                    <a class="footer__link" href="#!">About</a>
-                </li>
-                <li class="footer__links-item">
-                    <a class="footer__link" href="#!">History</a>
-                </li>
-                <li class="footer__links-item">
-                    <a class="footer__link" href="#!">License</a>
-                </li>
-            </ul>
-        </div>
-        <div >
-            <ul class="footer__links">
-                <h4 class="footer__head">Jarvis Products</h4>
-                <li class="footer__links-item">
-                    <a class="footer__link" href="#!">Core</a>
-                </li>
-                <li class="footer__links-item">
-                    <a class="footer__link" href="#!">Telegram</a>
-                </li>
-                <li class="footer__links-item">
-                    <a class="footer__link" href="#!">Discord</a>
-                </li>
-                <li class="footer__links-item">
-                    <a class="footer__link" href="#!">Slack</a>
-                </li>
-            </ul>
-        </div>
-        
-     </div>
-     <p class="copyright-text">Copyright &copy; 2020 Jarvis Web</p>
- </footer>
+  <footer>
+    <div class="footer">
+      <div>
+        <ul class="footer__links">
+          <h4 class="footer__head">Information</h4>
+          <li class="footer__links-item">
+            <a class="footer__link" href="#!">Home</a>
+          </li>
+          <li class="footer__links-item">
+            <a class="footer__link" href="#!">About</a>
+          </li>
+          <li class="footer__links-item">
+            <a class="footer__link" href="#!">History</a>
+          </li>
+          <li class="footer__links-item">
+            <a class="footer__link" href="#!">License</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul class="footer__links">
+          <h4 class="footer__head">Jarvis Products</h4>
+          <li class="footer__links-item">
+            <a class="footer__link" href="#!">Core</a>
+          </li>
+          <li class="footer__links-item">
+            <a class="footer__link" href="#!">Telegram</a>
+          </li>
+          <li class="footer__links-item">
+            <a class="footer__link" href="#!">Discord</a>
+          </li>
+          <li class="footer__links-item">
+            <a class="footer__link" href="#!">Slack</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <p class="copyright-text">Copyright &copy; 2020 Jarvis Web</p>
+  </footer>
 </template>
-
-
 
 <script>
 export default {
-  name: "Footer"
+  name: "Footer",
 };
 </script>
 
-
 <style scoped>
-
-footer{
-    background-color: #333;
-    color:white;
-    min-height:10vh;
+footer {
+  background-color: #173f5f;
+  color: white;
+  min-height: 10vh;
+  width: 100%;
+  position: absolute;
+  left: 0;
 }
-.footer{
-    padding-top:10px;
-    max-width:1140px;
-    margin:0 auto;
-    display:flex;
-    justify-content: space-around;
-    align-items: center;
-  
-}
-
-.footer__head{
-   padding-top:1rem;
-   letter-spacing: 2px;
-   
+.footer {
+  padding-top: 10px;
+  max-width: 1140px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 }
 
-.footer__links{
-    list-style: none;
-    display:flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-}
-.footer__links-item{
-    margin:0;
-    padding:.4rem;
-    align-self: flex-start;
+.footer__head {
+  padding-top: 1rem;
+  letter-spacing: 2px;
 }
 
-.footer__link:link,.footer__link:visited{
-    color:white;
-    text-decoration: none;
-    letter-spacing:2px;
-    font-size:1rem;
+.footer__links {
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
-.footer__link:active,.footer__link:hover{
-    color:#05728f;
-}
-.copyright-text{
-    padding:10px;
-    font-size:14px;
+.footer__links-item {
+  margin: 0;
+  padding: 0.4rem;
+  align-self: flex-start;
 }
 
-
+.footer__link:link,
+.footer__link:visited {
+  color: white;
+  text-decoration: none;
+  letter-spacing: 2px;
+  font-size: 1rem;
+}
+.footer__link:active,
+.footer__link:hover {
+  color: #05728f;
+}
+.copyright-text {
+  padding: 10px;
+  font-size: 14px;
+}
 </style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,128 +1,120 @@
 <template>
   <nav class="nav">
-      <h1 class="nav-header">Jarvis</h1>
-      <ul  v-bind:class="[isActive ? 'nav-links' : 'nav-links active']">
-        <li class="nav-link">
-          <router-link to="/" class="link">Home</router-link>
-        </li>
-        <li class="nav-link">
-          <router-link to="/about" class="link">About</router-link>
-        </li>
-        <li class="nav-link">
-          <router-link to="#!" class="link">Projects</router-link>
-        </li>
-      </ul>
-      <div class="burger" id="burger" @click="toggleClass()"> 
-        <span class="line"></span>
-        <span class="line"></span>
-        <span class="line"></span>
-      </div>
-    </nav> 
+    <h1 class="nav-header">Jarvis</h1>
+    <ul v-bind:class="[isActive ? 'nav-links' : 'nav-links active']">
+      <li class="nav-link">
+        <router-link to="/" class="link">Home</router-link>
+      </li>
+      <li class="nav-link">
+        <router-link to="/about" class="link">About</router-link>
+      </li>
+      <li class="nav-link">
+        <router-link to="#!" class="link">Projects</router-link>
+      </li>
+    </ul>
+    <div class="burger" id="burger" @click="toggleClass()">
+      <span class="line"></span>
+      <span class="line"></span>
+      <span class="line"></span>
+    </div>
+  </nav>
 </template>
-
 
 <script>
 export default {
   name: "Navigation",
-  data:function(){
-   return{
-     isActive:true
-   }
+  data: function () {
+    return {
+      isActive: true,
+    };
   },
   methods: {
-    toggleClass: function(){
+    toggleClass: function () {
       this.isActive = !this.isActive;
-    }
-  }
-}
-
+    },
+  },
+};
 </script>
-
 
 <style scoped>
 .nav {
   min-height: 10vh;
   background-color: #173f5f;
-  display:flex;
+  display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-bottom:1rem;
+  margin-bottom: 1rem;
 }
-.nav-header{
-  font-size:2rem;
-  color:#fff;
-  margin-left:2rem;
-  flex:0 0 1;
-  padding:1rem;
+.nav-header {
+  font-size: 2rem;
+  color: #fff;
+  margin-left: 2rem;
+  flex: 0 0 1;
+  padding: 1rem;
 }
 
-.nav-links{
-  display:flex;
+.nav-links {
+  display: flex;
   justify-content: center;
-  align-items:center;
-  margin:0rem 1rem;
-  flex:0 0 2;
-  margin-right:6rem;
+  align-items: center;
+  margin: 0rem 1rem;
+  flex: 0 0 2;
+  margin-right: 6rem;
 }
 
-
-.link:link,.link:visited{
-  color:#fff;
+.link:link,
+.link:visited {
+  color: #fff;
   text-decoration: none;
-  text-transform:uppercase;
-  letter-spacing:2px;
-
+  text-transform: uppercase;
+  letter-spacing: 2px;
 }
-.link:active,.link:hover{
-  color:rgb(165, 158, 158)
+.link:active,
+.link:hover {
+  color: rgb(165, 158, 158);
 }
-.burger{
-  display:block;
-  width:1.5rem;
-  height:1.5rem;
-  cursor:pointer;
-  position:absolute;
-  top:1.4rem;
-  right:5rem;
-  display:none;
-  
+.burger {
+  display: block;
+  width: 1.5rem;
+  height: 1.5rem;
+  cursor: pointer;
+  position: absolute;
+  top: 1.4rem;
+  right: 5rem;
+  display: none;
 }
-.line{
-  display:block;
-  padding:0.1rem 0rem;
-  width:1.4rem;
-  margin:0.3rem;
-  background-color:#fff;
- 
+.line {
+  display: block;
+  padding: 0.1rem 0rem;
+  width: 1.4rem;
+  margin: 0.3rem;
+  background-color: #fff;
 }
 
-@media only screen and (max-width:800px){
-  .burger{
-    display:block;
+@media only screen and (max-width: 800px) {
+  .burger {
+    display: block;
   }
-  .nav{
-    flex-direction:column;
-  }
-  
-  .nav-links{
-    flex-direction:column;
-    width:100%;
-    margin:0;
+  .nav {
+    flex-direction: column;
   }
 
-  .active.nav-links{
-    display:none;
+  .nav-links {
+    flex-direction: column;
+    width: 100%;
+    margin: 0;
+  }
+
+  .active.nav-links {
+    display: none;
   }
 }
 
-@media only screen and (max-width:400px){
-
-  .nav-header{
-    font-size:2rem;
-    margin-left:0.5rem;
+@media only screen and (max-width: 400px) {
+  .nav-header {
+    font-size: 2rem;
+    margin-left: 0.5rem;
     align-self: flex-start;
   }
 }
-
-
 </style>
